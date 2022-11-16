b6e01250f064dbf0eab2ca7cda76609465b1552c
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-16 12:28:23 +0000

    finished building restaurant and review models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_114641) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_122209) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_114641) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end

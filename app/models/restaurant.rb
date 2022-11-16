95fc519ff858251cf77d8615d3962b82f2f0aeb1
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-16 12:08:53 +0000

    made reviews dependent on restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   # must have name, address and a category
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :address, :category, presence: true

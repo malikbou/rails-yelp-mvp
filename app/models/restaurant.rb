f1279fb1995fc9fa9daaa2cacad2b7262b112b40
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-16 12:03:31 +0000

    added category validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   # must have name, address and a category
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
